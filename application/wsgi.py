3b5966bd3aa496b2507a2c5146df8eb881ff7d47
EJ-Cap <emilioaso@outlook.com>
2026-01-28 13:37:17 +0100

    Delete application directory

--- application/wsgi.py
+++
@@ -1,9 +0,0 @@
-"""Flask application entry point."""
-
-import os
-from app import create_app
-
-app = create_app(config_name=os.environ.get("FLASK_ENV", "development"))
-
-if __name__ == "__main__":
-    app.run()
